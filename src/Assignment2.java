46728957210f1f4e784c6bdbafc627c51c0378a0
Keaton Currie <kdcurrie@dons.usfca.edu>
2019-12-07 14:53:30 -0800

    Finished Project

--- src/Assignment2.java
+++ src/Assignment2.java
@@ -1,6 +1,87 @@
+import java.io.*;
+import java.util.*;
+import com.opencsv.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+
 public class Assignment2 {
 
+    public Assignment2(){
+    }
+
+    /**************
+     * Read File
+     **************/
+    public void read(String path, Graph graph){
+        try {
+            //create csvreader and jsonparser
+            FileReader fileReader = new FileReader(path);
+            CSVReader csvReader = new CSVReader(fileReader);
+            JSONParser jp = new JSONParser();
+
+
+            //empty string array to store file contents
+            String[] s;
+            //read each line and put into string
+            while((s = csvReader.readNext()) != null){
+                try {
+
+                    //creates graph containing actors and links
+                    ArrayList<String> actors = new ArrayList<>();
+                    JSONArray obj= (JSONArray) jp.parse(s[2]);
+
+                    for(int i = 0;i < obj.size(); i++){
+                        JSONObject temp = (JSONObject)obj.get(i);
+                        String name = (String)temp.get("name");
+                        actors.add(name);
+                        for (Object o : obj) {
+                            JSONObject tempJ = (JSONObject) o;
+                            String nameJ = (String) tempJ.get("name");
+                            if (!name.toLowerCase().equals(nameJ.toLowerCase())) {
+                                graph.addEgde(name.toLowerCase(), nameJ.toLowerCase());
+                            }
+
+                        }
+
+                    }
+
+
+                }
+                catch (Exception e){
+                    continue;
+                }
+
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+    /*****************
+     * Main Function
+     *****************/
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Assignment2 test = new Assignment2();
+        Graph graph = new Graph();
+        System.out.println("Processing file to Create Graph");
+        test.read(args[0], graph);
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Actor 1: ");
+        String name1 = scan.nextLine().toLowerCase();
+        System.out.print("Actor 2: ");
+        String name2 = scan.nextLine().toLowerCase();
+        graph.BFS(name1, name2);
+
+
+
+
+
     }
 }
+
+
+
--- src/Graph.java
+++ src/Graph.java
@@ -1,3 +1,110 @@
+import java.util.*;
+
+/*****************
+ * Graph Class
+ ****************/
 public class Graph {
-    
+    HashMap<String, Set<String>> graph;
+    //Graph constructor
+    public Graph() {
+        graph = new HashMap<>();
+    }
+    //Add edges
+    public void addEgde (String a1, String a2) {
+        if(a1.equals(a2)){
+            return;
+        }
+        else{
+            //Get node and create link to other node
+            if(graph.containsKey(a1)){
+                graph.get(a1).add(a2);
+            }
+            else {
+                Set<String> h1 = new HashSet<>();
+                h1.add(a2);
+                graph.put(a1,h1);
+
+            }
+            if(graph.containsKey(a2)){
+                graph.get(a2).add(a1);
+            }
+            else{
+
+                Set<String> h2 = new HashSet<>();
+                h2.add(a1);
+                graph.put(a2,h2);
+            }
+
+        }
+
+
+    }
+    /***********************
+     * Breadth First Search
+     ***********************/
+    public void BFS(String start, String end) {
+        if (!graph.containsKey(start) || !graph.containsKey(end)) {
+            System.out.println("No Such Actor");
+            return;
+        } else {
+
+
+            ArrayList<String> lookup = new ArrayList<>();
+            LinkedList<String> queue = new LinkedList<>();
+            Hashtable<String, String> path = new Hashtable<>(); //To hold the correct path
+            path.put(start, "p"); //put start path to placeholder
+            queue.add(start);
+            lookup.add(start);
+            while (!queue.contains(end)) {
+                String actor = queue.removeFirst();
+                //System.out.println(actor);B
+                for (String contact : graph.get(actor)) {
+                    //add path
+                    if (!lookup.contains(contact)) {
+                        lookup.add(contact);
+                        queue.add((contact));
+                        path.put(contact,actor);
+                        //end if you got path
+                        if (contact.equals(end)) {
+                            System.out.print("Shortest Path between " + start + " and " + end + ": ");
+                            if (!path.containsKey(end)) {
+                                path.put(end, actor);
+                            }
+
+
+                        }
+
+
+                    }
+
+                }
+            }
+            //Put path into stack and pop to print in correct order
+            String person = path.get(end);
+            Stack<String> shortPath = new Stack<>();
+
+            do {
+                shortPath.push(person);
+                person = path.get(person);
+
+            } while (!person.equals("p"));
+
+            while(!shortPath.isEmpty()){
+                if(!person.equals(end)){
+                    person = shortPath.pop();
+                    System.out.print(person + " -> ");
+                }
+                else {
+                    System.out.print(person);
+                }
+
+            }
+            System.out.print(end);
+        }
+    }
+
 }
+
+
+
+
